bf3de9f4ff13dc78884dc64179fecf6983e118f5
mnixon325 <michael.nixon32598@gmail.com>
2019-02-23 21:46:46 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,20 +11,6 @@ class MoviesController < ApplicationController
   end
 
   def index
-    #@movies = Movie.all
-    #@movies = if params[:sort_by] == "title" 
-    #  Movie.order(:title)
-    #  @hilite = Movie.order(:title)
-    #else
-    #  Movie.all
-    #end
-    
-    #@movies = if params[:sort_by] == "release date" 
-    #  Movie.order(:release_date)
-    #else
-    #  Movie.all
-    #end
-    
     @movies = Movie.all
     @movies = if params[:sort_by] == "title"
                 Movie.order(:title)
