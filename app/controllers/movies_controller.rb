f20eddfd433ee068c6dbd3adb4a0eb37b1b4225e
mnixon325 <michael.nixon32598@gmail.com>
2019-02-23 06:04:05 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -19,7 +19,7 @@ class MoviesController < ApplicationController
       Movie.all
     end
     
-    @movies = ReleaseDate.all
+    @movies2 = ReleaseDate.all
     @movies = if params[:sort_by] == "release date" 
       ReleaseDate.order(:release_date)
     else
