7252be91faa4d2537b560e7df8789f32801ee8f5
mnixon325 <michael.nixon32598@gmail.com>
2019-02-23 06:51:41 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -22,7 +22,7 @@ class MoviesController < ApplicationController
     @movies = if params[:sort_by] == "release date" 
       Movie.order(:release_date)
     else
-      Movie.sort
+      Movie.default
     end
   end
 
