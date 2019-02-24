eb03366cf37ac9dbc5028d3611baef47778c7b1e
mnixon325 <michael.nixon32598@gmail.com>
2019-02-24 07:06:31 +0000

    part 2 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,7 @@
 class Movie < ActiveRecord::Base
     
     def self.all_rating
-        return %w[G PG PG-13 R]
-        #return arr
+        arr = ['G', 'PG', 'PG-13', 'R']
+        return arr
     end
 end
